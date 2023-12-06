89b034e57ea9d5a945e76f213552787ca78241c6
Stefanos-Iordanis Papadopoulos <stefpapad@iti.gr>
2023-12-06 10:42:30 +0200

    Add missing function

--- src/prepare_evidence.py
+++ src/prepare_evidence.py
@@ -1,14 +1,15 @@
 import os
 import json
 import torch
+import faiss
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
 from string import digits
+from ast import literal_eval
 from utils import load_visual_news, load_features, load_training_dataset
 from utils_evidence import (fetch_evidence_split, load_merge_evidence_w_data, str_to_list, process_string, idx_captions, idx_images, prepare_source_dataloaders)
 from lavis.models import load_model_and_preprocess
-from torch.utils.data import DataLoader
 from tqdm import tqdm
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -250,6 +251,98 @@ def rank_X_items(input_df, image_cols, text_cols, X_image_embeddings, X_text_emb
     return pd.DataFrame(img_most_similar_items), pd.DataFrame(txt_most_similar_items)
 
 
+def build_faiss_index(data):
+    index = faiss.IndexFlatL2(data.shape[1])  # L2 distance (Euclidean)
+    index.add(data)
+    return index
+
+def find_similar_row_indices(query_row, index, k):
+    D, I = index.search(query_row.reshape(1, -1), k)
+    similar_row_indices = I[0]
+
+    return similar_row_indices
+
+def calculate_most_similar(input_data, split_name, image_embeddings, text_embeddings, evidence_path, dataset_name, encoder, encoder_version, K=12):
+    
+    index_images = build_faiss_index(image_embeddings[input_data.image_id].values.T)
+    index_texts = build_faiss_index(text_embeddings[input_data.id].values.T)
+    
+    all_similar_items = []
+
+    for i, sample in tqdm(input_data.iterrows(), total=input_data.shape[0]):    
+
+        similar_rows_images = find_similar_row_indices(image_embeddings[sample.image_id].values, index_images, K)    
+        similar_rows_texts = find_similar_row_indices(text_embeddings[sample.id].values, index_texts, K)    
+        
+        keep_ids = []
+        keep_image_ids = []
+        
+        for j in similar_rows_images[1:]:
+            neg_sample = input_data.iloc[j]
+            neg_sample_id = neg_sample.id
+            neg_sample_image_id = neg_sample.image_id 
+            if neg_sample_id != sample.id and neg_sample_image_id != sample.image_id:
+                keep_image_ids.append(neg_sample_image_id)
+
+        for j in similar_rows_texts[1:]:
+            neg_sample = input_data.iloc[j]
+            neg_sample_id = neg_sample.id
+            neg_sample_image_id = neg_sample.image_id 
+            if neg_sample_id != sample.id and neg_sample_image_id != sample.image_id:
+                keep_ids.append(neg_sample_id)                
+                
+        all_similar_items.append({
+            'id': sample.id,
+            'image_id': sample.image_id,        
+            'most_similar_id': keep_ids,
+            'most_similar_image_id': keep_image_ids,        
+        })
+
+        out_path = evidence_path + dataset_name + "_most_similar_items_" + split_name + "_" + encoder.replace("-", "").replace("/", "").lower() + "_" + encoder_version.replace("-", "").replace("/", "").lower()
+        pd.DataFrame(all_similar_items).to_csv(out_path + ".csv")
+
+
+def prepare_negative_evidence(input_data, split_name, evidence_path, dataset_name, encoder, encoder_version):
+
+    all_negative_texts = []
+    all_negative_images = []
+
+    for (i, row) in tqdm(input_data.iterrows(), total=input_data.shape[0]):
+
+        img_match_index = None
+        txt_match_index = None
+
+        negative_texts = []
+        negative_images = []
+
+        most_similar_ids = literal_eval(row.most_similar_id)
+        most_similar_image_ids = literal_eval(row.most_similar_image_id)    
+
+        for most_similar_id in most_similar_ids[:5]:
+
+            most_similar_id_item = input_data[input_data.id == most_similar_id].iloc[0]
+            img_match_index = [x for x in most_similar_id_item.img_ranked_items if x.startswith(most_similar_id_item.match_index + '_')]
+            negative_images.extend(img_match_index)
+
+        for most_similar_image_id in most_similar_image_ids[:5]:
+
+            most_similar_image_id_item = input_data[input_data.image_id == most_similar_image_id].iloc[0]
+            txt_match_index = [x for x in most_similar_image_id_item.txt_ranked_items if x.startswith(most_similar_image_id_item.match_index + '_')]
+            negative_texts.extend(txt_match_index)
+
+        all_negative_texts.append(negative_texts)    
+        all_negative_images.append(negative_images)
+
+    out_negative_images_path = evidence_path + dataset_name + "_negative_images_" + split_name + "_" + encoder.replace("-", "").replace("/", "").lower() + "_" + encoder_version.replace("-", "").replace("/", "").lower() + ".json"
+    out_negative_texts_path = evidence_path + dataset_name + "_negative_texts_" + split_name + "_" + encoder.replace("-", "").replace("/", "").lower() + "_" + encoder_version.replace("-", "").replace("/", "").lower() + ".json"
+
+    with open(out_negative_images_path, 'w') as file:
+        json.dump(all_negative_images, file)
+
+    with open(out_negative_texts_path, 'w') as file:
+        json.dump(all_negative_texts, file)
+        
+
 def re_rank_evidence(data_path, data_name, data_name_X, output_path, encoder='CLIP', choose_encoder_version='ViT-B/32'):
     print("Load data")
     vn_data = load_visual_news(data_path)
@@ -332,8 +425,24 @@ def re_rank_evidence(data_path, data_name, data_name_X, output_path, encoder='CL
                                                          )
     train_data = pd.concat([train_data, train_ranked_X_img], axis=1)
     train_data = pd.concat([train_data, train_ranked_X_txt], axis=1)
-    train_data.to_csv(data_path + 'news_clippings/merged_balanced_train_ranked_' + encoder.lower() + "_" + encoder_version + '.csv')
-    
+    train_data.to_csv(data_path + 'news_clippings/merged_balanced_train_ranked_' + encoder.lower() + "_" + encoder_version + '.csv')  
+
+    calculate_most_similar(train_data, "train", image_embeddings, text_embeddings, output_path, data_name, encoder, encoder_version,K=12)
+    calculate_most_similar(valid_data, "valid", image_embeddings, text_embeddings, output_path, data_name, encoder, encoder_version,K=12)
+    calculate_most_similar(test_data, "test", image_embeddings, text_embeddings, output_path, data_name, encoder, encoder_version,K=12)      
+
+    train_data[["most_similar_id", "most_similar_image_id"]] = pd.read_csv(output_path + data_name + "_most_similar_items_" + "train" + "_" + encoder.replace("-", "").replace("/", "").lower() + "_" + encoder_version.replace("-", "").replace("/", "").lower()
+    + ".csv", index_col=0)[["most_similar_id", "most_similar_image_id"]]
+
+    valid_data[["most_similar_id", "most_similar_image_id"]] = pd.read_csv(output_path + data_name + "_most_similar_items_" + "valid" + "_" + encoder.replace("-", "").replace("/", "").lower() + "_" + encoder_version.replace("-", "").replace("/", "").lower()
+    + ".csv", index_col=0)[["most_similar_id", "most_similar_image_id"]]
+
+    test_data[["most_similar_id", "most_similar_image_id"]] = pd.read_csv(output_path + data_name + "_most_similar_items_" + "test" + "_" + encoder.replace("-", "").replace("/", "").lower() + "_" + encoder_version.replace("-", "").replace("/", "").lower()
+    + ".csv", index_col=0)[["most_similar_id", "most_similar_image_id"]]
+
+    prepare_negative_evidence(train_data, "train", output_path, data_name, encoder, encoder_version)
+    prepare_negative_evidence(valid_data, "valid", output_path, data_name, encoder, encoder_version)
+    prepare_negative_evidence(test_data, "test", output_path, data_name, encoder, encoder_version)
     
 def re_rank_verite(data_path, data_name, output_path, encoder='CLIP', choose_encoder_version='ViT-B/32'):
     
